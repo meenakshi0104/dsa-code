8a18ac2ce4eeacc7092a52f246e70d0412907362
Meenakshi Singh <meenakshi01042000@gmail.com>
2023-11-29 11:27:44 +0530

    Optimise Approach of Question6

--- Question6.cpp
+++ Question6.cpp
@@ -12,23 +12,35 @@ void solve(){
     for(int i=0; i<n; i++){
         cin >> arr[i];
     }
+
+    // Using Sorting O(nlogn) 
     sort(arr,arr+n);
     int flag = 0;
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (arr[i] == arr[i + 1])
-        {
+    for (int i = 0; i < n - 1; i++){
+        if (arr[i] == arr[i + 1]){
             flag = 1;
             break;
-        }
-        
+        }   
     }
     if(flag == 1){
         cout<<"True";
-    }
-    else{
+    }else{
         cout<<"False";
     }
+
+    // Using Set O(n)
+    set<int> s;
+    for (int i = 0; i < n; i++)
+    {
+        s.insert(arr[i]);
+    }
+    if (s.size() < n)
+    {
+        cout<<"True";
+    }else{
+        cout<<"False";
+    }
+  
 }
 
 int main(){
