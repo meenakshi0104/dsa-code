2cd7608d10fceee836a6fd78e7cde558ec4efa29
Meenakshi Singh <meenakshi01042000@gmail.com>
2023-11-29 11:24:32 +0530

    Question6 of DSA series

---
+++ Question6.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+
+    // Contains Duplicate
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    sort(arr,arr+n);
+    int flag = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] == arr[i + 1])
+        {
+            flag = 1;
+            break;
+        }
+        
+    }
+    if(flag == 1){
+        cout<<"True";
+    }
+    else{
+        cout<<"False";
+    }
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+
+}
